be60f2944ae66a61309c54f4719fa0c5d44078af
GMalinga1 <gmalinga718@gmail.com>
2023-05-24 13:10:37 +0530

    Flutter BMI Calculator Vertion 1.0

---
+++ lib/const.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+const ktextcolor = Color(0xFFFF8888);
+const koutputTextcolor = Color.fromARGB(255, 164, 8, 8);
+const kInputLableColor =
+    TextStyle(color: ktextcolor, fontSize: 50, fontWeight: FontWeight.bold);
+                        // ignore: prefer_typing_uninitialized_variables
+                        
